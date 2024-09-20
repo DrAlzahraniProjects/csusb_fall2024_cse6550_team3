37847100b5629c1ed9efcf8c691c4d17d28c6ca1
Vamsikishore-hub <008743690@coyote.csusb.edu>
2024-09-20 11:25:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,5 @@ def main():
 	st.title("Hello World! - Team 3")
 
 if __name__ == "__main__":
-	# If streamlit instance is running
-	if os.environ.get("STREAMLIT_RUNNING") == "1":
 		main()
-
-	# If streamlit is not running
-	else:
-		os.environ["STREAMLIT_RUNNING"] = "1" # Set the environment variable to indicate Streamlit is running
-		subprocess.run(["streamlit", "run", __file__, "--server.port=5003", "--server.address=0.0.0.0"])
+	
