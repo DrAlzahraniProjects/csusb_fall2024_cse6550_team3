371572ef9fd3de78496db2e0b5e1cc1d52a2bf8f
Vamsikishore-hub <008743690@coyote.csusb.edu>
2024-09-23 18:33:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-import subprocess
 
 def main():
     """Main Streamlit app logic."""
@@ -58,13 +57,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # If streamlit instance is running
-    if os.environ.get("STREAMLIT_RUNNING") == "1":
-        main()
-
-    # If streamlit is not running
-    else:
-        os.environ["STREAMLIT_RUNNING"] = "1"  # Set the environment variable to indicate Streamlit is running
-        subprocess.Popen(["streamlit", "run", __file__, "--server.port=5003", "--server.address=0.0.0.0"])
-        subprocess.Popen(["service", "nginx", "start"])
-        subprocess.run(["jupyter", "notebook", "--ip=0.0.0.0", "--port=6003", "--no-browser", "--allow-root"])
+    main()
